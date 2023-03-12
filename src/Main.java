ad1600abd964e37c7499635fcbcbbbda77d7378e
Nikolay <vnovus46@gmail.com>
2023-03-12 22:34:20 +0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int amount = 20;
+        int price = 7500;
+        System.out.println("колличество бонусных миль- "+(price/amount));
+
+
     }
 }
